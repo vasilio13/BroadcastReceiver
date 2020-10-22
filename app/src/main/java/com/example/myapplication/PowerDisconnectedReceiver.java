6b9783db6e15bed60d16c55f6758c17582d8c52c
vasilio13 <vasilkhaliaukin@gmail.com>
2020-10-22 13:01:31 +0300

    completed

--- app/src/main/java/com/example/myapplication/PowerDisconnectedReceiver.java
+++ app/src/main/java/com/example/myapplication/PowerDisconnectedReceiver.java
@@ -3,17 +3,11 @@ package com.example.myapplication;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.widget.Toast;
 
-import static android.widget.Toast.LENGTH_LONG;
-
 public class PowerDisconnectedReceiver extends BroadcastReceiver {
-    //private static final String TAG = "StartupReceiver";
-
     @Override
     public void onReceive(Context context, Intent intent) {
-       // Log.i(TAG,"Received broadcast intent: " + intent.getAction());
         Toast.makeText(context, "Power Disconnected",
                 Toast.LENGTH_LONG).show();
     }
